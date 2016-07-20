126ac20a9c34037ae7ff70c5d320f50501cbca19
nopain1981 <jalinski.w@bluewin.ch>
2016-07-20 10:00:01 +0200

    version: Mi 20. Jul 10:00:01 CEST 2016

--- INSTALL/build_all.sh
+++ INSTALL/build_all.sh
@@ -6,4 +6,5 @@
 ./compile.and.create.sh sonarr 8989:8989
 ./compile.and.create.sh tvheadend 9981:9981
 ./compile.and.create.sh batchrename 8080:8080
-./compile.and.create.sh batchrename 6789:6789
+#./compile.and.create.sh batchrename 6789:6789
+./compile.and.create.sh noip 1234:1234
