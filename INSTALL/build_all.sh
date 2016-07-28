513d2e8fbb4e23d85e13b95c75783639f8412c7e
nopain1981 <jalinski.w@bluewin.ch>
2016-07-28 02:00:01 +0200

    version: Do 28. Jul 02:00:01 CEST 2016

--- INSTALL/build_all.sh
+++ INSTALL/build_all.sh
@@ -1,3 +1,17 @@
+#/bin/bash
+# this script supports the drop and recreate of all docker containers & images
+# process: 
+# 1) kill current running container & rm the container
+# 2) remove the old image
+# 3) recreate the image and build container
+#
+#------------------------------------------
+# lets start with couchpotato
+for cc in `docker ps -a | awk {'print $2'} | grep -v ID`;
+do
+docker kill $cc && docker rm $cc
+docker rmi -f $cc
+done
 ./compile.and.create.sh couchpotato 5050:5050
 ./compile.and.create.sh headphones 5053:5053
 ./compile.and.create.sh oscam 16999:16999
