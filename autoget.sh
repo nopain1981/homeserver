f9660f3ef343ebb028fbf3d0d22e0220864ae948
nopain1981 <jalinski.w@bluewin.ch>
2016-08-08 10:25:04 +0200

    version: Mo 8. Aug 10:25:04 CEST 2016

--- autoget.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-cd /playground/homeserver \
-&& git fetch --all \
-&& git reset --hard origin/master \
-&& git pull origin master
-echo "Last Refresh occoured: `date`" > /playground/last_pull.txt
