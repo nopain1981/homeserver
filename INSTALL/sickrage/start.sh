e8a57f3ef7aa563d09d107d9f89c7b82254d2bfa
Werner Jalinski <jalinski.w@bluewin.ch>
2016-11-07 23:10:01 +0100

    Version Mon Nov  7 23:10:01 CET 2016

--- INSTALL/sickrage/start.sh
+++ INSTALL/sickrage/start.sh
@@ -17,13 +17,14 @@ chown $PUSER:$PGROUP -R /tmp
 echo "[INFO] Checkout the latest SickRage version ..."
 [[ ! -d $SR_HOME/app/.git ]] && gosu $PUSER:$PGROUP bash -c "git clone -b $SR_BRANCH $SR_REPO $SR_HOME/app"
 
-pip install -r /sickrage/app/requirements.txt
-
 # opt out for autoupdates using env variable
 if [ -z "$ADVANCED_DISABLEUPDATES" ]; then
         echo "[INFO] Autoupdate is active, try to pull the latest sources ..."
 	# update the application
 	cd $SR_HOME/app/ && gosu $PUSER:$PGROUP bash -c "git pull"
+        pip install -r /sickrage/app/requirements.txt
+        pip uninstall pyopenssl
+	pip install pyopenssl==0.15
 fi
 
 # run SickRage
